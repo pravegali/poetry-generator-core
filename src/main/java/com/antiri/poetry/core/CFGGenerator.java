fa10c349f4fec09ec340adddc82226c697c3623d
Chris Hilla <christopher.hilla@gmail.com>
2015-08-05 13:22:30 -0400

    The previous approach will not work. New approach.
    We can't handle $ signs without escaping them.
    We can't take multiple sets of expressions, such as
      verb1|verb2|verb3 verba|verbb|verbc should = verb1 verbc or
      something like that, because of the space.
    
    Can't handle executing commands because the poem is
    processed by searching for a pair of angle brackets, and
    detecting a command would immediatly cause the program to
    exit when we really want it to process all the text up to
    the command. Ex : verb1|verb2|verb3|$END means we'll just
    exit immediatly and not consider a random selection of those
    verbs.

--- src/main/java/com/antiri/poetry/core/CFGGenerator.java
+++ src/main/java/com/antiri/poetry/core/CFGGenerator.java
@@ -1,8 +1,13 @@
 package com.antiri.poetry.core;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -13,6 +18,67 @@ public class CFGGenerator {
 
     public static void main(String[] args) throws IOException {
 
+        /*
+         * Read input file, generate a production, one for each line
+         *   A production is a map, the key is the non-terminal id and the key is a list of expressions, which
+         *   could in turn be other non-terminals, or also could be commands.
+         *
+         * The root non-terminal is POEM (hard code that), so start with that and substitute it by looking
+         * in the map for that key, and selecting at random an expression.
+         *
+         * Split up the expression by whitespace and put each group into a list.
+         *   Ex: This is a <ADJECTIVE> one $END Can't get here
+         *   This breaks down into {This is a }, <ADJECTIVE>, { one }, $END, " Can't get here"
+         *   Notice the spaces are included
+         *
+         * Iterate ove this list.
+         * If it contains something in angle brackets, it's an expression. Go out and fetch at random another one from the production map
+         * If it starts with a $ symbol, it's a command. Execute the command immediatly.
+         * Else, it's terminal, substitute it.
+         *
+         * Example:
+         * POEM: <LINE> <LINE> <LINE> $END
+         * LINE: <VERB> you <ADJECTIVE> <NOUN> $LINEBREAK
+         * VERB: jump|swim|run
+         * ADJECTIVE: silly|foolish|<NOUN> loving
+         * NOUN: apple-jack|mongoose|hacker
+         *
+         * POEM
+         * <LINE> <LINE> <LINE>
+         * <VERB> you <ADJECTIVE> <NOUN> \n <LINE> <LINE>
+         * run you <ADJECTIVE> <NOUN> \n <LINE> <LINE>
+         * run you <NOUN> loving <NOUN> \n <LINE> <LINE>
+         * run you apple-jack loving <NOUN> \n <LINE> <LINE>
+         * run you apple-jack loving mongoose \n <LINE> <LINE>
+         *     etc etc etc
+         *
+         *
+         */
+
+        Map<String, List<String>> productions = new HashMap<String, List<String>>();
+
+        File cfgFile = new File("poem.txt");
+        Scanner fileScanner = new Scanner(cfgFile);
+
+        while (fileScanner.hasNext()) {
+            String production = fileScanner.nextLine();
+            List<String> expressions = new ArrayList<String>();
+            String nonTerminal = production.split(": ")[0];
+            expressions.addAll(Arrays.asList(production.split(": ")[1].split("\\|")));
+            productions.put(nonTerminal, expressions);
+        }
+
+        System.out.println(productions);
+    }
+
+    /**
+     * Replace all instances of $LINEBREAK with a newline
+     * Working from left to right, identify a non-terminal, look up a random expression and replace it.
+     *
+     * Problem here
+     * @throws FileNotFoundException
+     */
+    public static void initialAttempt() throws FileNotFoundException {
         String result = "<POEM>";
         File cfgFile = new File("poem.txt");
         Scanner fileScanner = new Scanner(cfgFile);
